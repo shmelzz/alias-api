8b7c33b43fbccf4d9822089b65bb60e4bf076971
Elizaveta Shelemekh <shelemekhliza0313@gmail.com>
2023-03-28 19:32:09 +0300

    add root to get room by id

--- Sources/App/Controllers/RoomController.swift
+++ Sources/App/Controllers/RoomController.swift
@@ -14,9 +14,6 @@ struct RoomController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let roomsRoute = routes.grouped("rooms")
         
-        // roomsRoute.get(":roomId", use: getDinner)
-        // roomsRoute.put(":dinnerId", "invite", ":userId", use: inviteUser)
-        
         roomsRoute.post("new") { req -> EventLoopFuture<Room> in
             let room = try req.content.decode(Room.self)
             return room.save(on: req.db).map { room }
@@ -29,6 +26,12 @@ struct RoomController: RouteCollection {
         roomsRoute.get("public") { req -> EventLoopFuture<[Room]> in
             return Room.query(on: req.db).filter(\.$isPublic == true).all()
         }
+        
+        roomsRoute.get(":id") { req -> EventLoopFuture<Room> in
+            let roomId = req.parameters.get("id", as: UUID.self)
+            return Room.find(roomId, on: req.db).unwrap(or: Abort(.notFound))
+        }
+        
         
     }
 }
